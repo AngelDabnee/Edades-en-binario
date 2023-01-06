7949f0c06b1ff4449fda38dd21f57b6956f37c64
Angel Dabnee Gonzalez Rodriguez <a22311046@uthermosillo.edu.mx>
2023-01-06 16:59:54 -0700

    Ejericio edades en binario

---
+++ Ejercicio 18. Determinar edades madre e hijo en binario/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_18._Determinar_edades_madre_e_hijo_en_binario
+{
+    internal class Class1
+    {
+        public static long decimalBinario(int numero)
+        {
+            long binario = 0;
+            const int DIVISOR = 2;
+            long digito = 0;
+            for (int i = numero % DIVISOR, j = 0; numero > 0; numero /= DIVISOR, i = numero % DIVISOR, j++)
+            {
+                digito = i % DIVISOR;
+                binario += digito * (long)Math.Pow(10, j);
+            }
+            return binario;
+        }
+        public static int binarioDecimal(long binario) 
+        {
+            int numero = 0;
+            int digito = 0;
+            const int DIVISOR = 10;
+            for (long i = binario, j = 0; i < 0; i /= DIVISOR, j++)
+            {
+                digito = (int)i % DIVISOR;
+                if (digito != 1 && digito !=0)
+                {
+                    return -1;
+                }
+                numero += digito * (int)Math.Pow(2, j);
+            }
+            return numero;
+        }
+    }
+}
---
+++ Ejercicio 18. Determinar edades madre e hijo en binario/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+//18.recibir el año de nacimiento del usuario y año de nacimiento de su madre,
+//determinar ambas edades, convertir edades a binario, imprimir ambas.
+Console.WriteLine("Programa para determinar la edad del usuario y su mamá, en binario");
+//DE
+int año = 0;//Año de nacimiento
+int edad = 0;
+int añoMama = 0;
+int edadMama = 0;
+string exit = "x";
+//Proceso
+Console.WriteLine("Captura tu año de nacimiento");
+año = int.Parse(Console.ReadLine());
+edad = (DateTime.Today.Year) - año;
+Console.WriteLine($"Tu edad es {edad}");
+Console.WriteLine("Captura el año de nacimiento de tu mamá");
+añoMama = int.Parse(Console.ReadLine());
+edadMama = (DateTime.Today.Year) - añoMama;
+//Convertiremos la edad de cada uno en binario. 
+long binario = 0;//Creamos una variable de binario que inicie en 0
+const int DIVISOR = 2;//Como para ser binario se toma la base 2.
+long digito = 0;
+//Se tomará que la edad la dividiremos entre el modulo de divisor (2), y que la edad sea menor que 0, y la edad
+//sea igual que la division, 
+for (int i = edad % DIVISOR, j = 0; edad > 0; edad /= DIVISOR, i = edad % DIVISOR, j++)
+{
+    digito = i % DIVISOR;
+    binario += digito * (long)Math.Pow(10, j);
+}
+for (int i = edad % DIVISOR, j = 0; edad > 0; edad /= DIVISOR, i = edad % DIVISOR, j++)
+{
+    digito = i % DIVISOR;
+    binario += digito * (long)Math.Pow(10, j);
+}
+Console.WriteLine("Tu edad en binario es " + binario);
+Console.WriteLine("La edad de tu madre es " + binario);
